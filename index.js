1529f218913522ce2b0bddf9a4a154338b316989
Wesley Ozebe <wesleysantos345@hotmail.com>
2022-03-28 17:16:27 -0300

    Adicionado arquivos do projeto

---
+++ index.js
@@ -0,0 +1,18 @@
+const fs = require('fs');
+const csv = require('csvtojson'); //importa a lib para converter o csv para json
+
+const csvPath = './input.csv';
+const outputPath = './output.json';
+
+//carrega o arquivo csv do caminho acima.
+csv().fromFile(csvPath)
+.then((jsonObj) =>{
+    //salva o objeto em um .json.
+    fs.writeFile(outputPath, JSON.stringify(jsonObj), 'utf-8', function(err){
+        if(err){
+            console.log("Não foi possível salvar o objeto para o arquivo output.json!");
+            return console.log(err);
+        }
+        console.log("Arquivo salvo com sucesso!");
+    })
+});
